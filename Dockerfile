295ba276bd3064dbe5b19004752bbd3123a893dc
Detectiveboy007 <Detectiveboy007@DESKTOP-B0SGNHE>
2021-01-18 20:40:14 +0700

    first commit

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM node:12.18.4
+ 
+WORKDIR /app
+ 
+COPY package.json package.json
+COPY package-lock.json package-lock.json
+ 
+RUN npm install
+ 
+COPY . .
+ EXPOSE 80
+ EXPOSE 443
+CMD [ "node", "server.js" ]
---
+++ routes/index.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+var JsBarcode = require('jsbarcode');
+var Canvas = require("canvas");
+
+/* GET home page. */
+router.get('/:taxId/:suffix/:ref1/:ref2/:amount',(req, res, next) => {
+//creat variable for barcode data
+const textId = req.params.taxId;
+const suffix = req.params.suffix;
+const ref1 = req.params.ref1;
+const ref2 = req.params.ref2;
+const amount = req.params.amount;
+//Assign a barcode Data
+var barcodeData ="|";
+barcodeData += textId;
+barcodeData += suffix + "\r";
+barcodeData += ref1 + "\r";
+barcodeData += ref2 + "\r";
+barcodeData += amount ;
+
+// create canvas for draw barcode image
+var canvas = new Canvas.createCanvas();
+
+//generate barcode image
+JsBarcode(canvas,barcodeData);
+//set return type is jpg file
+res.contentType('image/jpeg');
+//respose to apps
+res.end(canvas.toBuffer());
+
+});
+
+module.exports = router;
